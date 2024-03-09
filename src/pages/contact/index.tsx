5bffc212d75c338f412249e3aa47953c6e9484fb
miahsu <Mia.hsu@tpisoftware.com>
2024-03-09 15:59:14 +0800

    test GSAP

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,15 +1,12 @@
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { gsap } from 'gsap/dist/gsap';
+import gsap from 'gsap/dist/gsap';
 import { useRef } from 'react';
-
-gsap.registerPlugin(ScrollTrigger);
+import { useIsomorphicLayoutEffect } from 'react-use';
 
 export default function Contact() {
   const main = useRef<HTMLDivElement>(null);
 
-  useGSAP(
-    () => {
+  useIsomorphicLayoutEffect(() => {
+    const ctx = gsap.context(() => {
       const boxes: HTMLDivElement[] = gsap.utils.toArray('.box');
       boxes.forEach((box) => {
         gsap.to(box, {
@@ -22,35 +19,22 @@ export default function Contact() {
           },
         });
       });
-    },
-    { scope: main }
-  );
+    }, main);
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <div ref={main}>
-      <section className='section flex-center column blue'>
+    <div>
+      <section className='section flex-center column'>
         <h1>Basic ScrollTrigger with React</h1>
         <h2>Scroll down to see the magic happen!!</h2>
       </section>
-      <div className='section flex-center column'>
-        <h1>This boxes animates as you scroll!</h1>
+      <div className='section flex-center column' ref={main}>
         <div className='box'>box</div>
         <div className='box'>box</div>
         <div className='box'>box</div>
       </div>
-      <section className='section flex-center orange column'>
-        <h1>The End!</h1>
-        <h2>
-          For more information visit the&nbsp;
-          <a
-            href='https://gsap.com/docs/v3/Plugins/ScrollTrigger'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Docs
-          </a>
-        </h2>
-      </section>
+      <section className='section'></section>
     </div>
   );
 }

2005fe31067e9870e1238455eaa7eab56c660bf3
miahsu <Mia.hsu@tpisoftware.com>
2024-03-09 15:34:32 +0800

    test GSAP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
 import AppLayout from '@/components/AppLayout';
 import '@/styles/main.css';
-import { useGSAP } from '@gsap/react';
-import { gsap } from 'gsap/dist/gsap';
-import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import type { AppProps } from 'next/app';
 
-if (typeof window !== 'undefined') {
-  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
-}
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppLayout>

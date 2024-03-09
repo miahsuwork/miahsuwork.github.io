d3bb7abc642c3025e4eede7fb00d80b7cc8b2fb9
miahsu <Mia.hsu@tpisoftware.com>
2024-03-09 12:09:46 +0800

    fix import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import AppLayout from '@/components/AppLayout';
 import '@/styles/main.css';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import type { AppProps } from 'next/app';
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
 
